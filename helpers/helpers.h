92c3d40b94f6abd02f6bb93676c9a4a2e5db9acc
charith jayarangana <cj.ptsz@yahoo.com>
2024-08-15 15:18:18 +0530

    create: helper functions header file for use

---
+++ helpers/helpers.h
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+// #include<math.h>
+
+#ifndef HELPERS_H
+
+#define HELPERS_H
+
+int charTonum(char x);
+
+int numTochar(int x);
+
+
+int char_toint(char *num);
+
+char* int_Tochar(int x);
+
+
+int stringtoenum(char *arr);
+
+#endif  //HELPERS_H
